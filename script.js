77fcf2f91f8f0f3a2292bff99215411e4c81e59a
Astha Negi <75171246+AsthaNegi@users.noreply.github.com>
2026-03-18 22:41:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-//your code here!
 
+
+const list=document.getElementById("infi-list");
+let count=1;
+
+function addItems(num){
+ for(let i=0;i<num;i++){
+    const listItem=document.createElement("li");  
+    listItem.textContent=`Item ${count++}`;
+    list.appendChild(listItem);
+ }   
+}
+
+addItems(10);
+
+window.addEventListener("scroll",()=>{
+    if(window.innerHeight+window.scrollY>=document.body.offsetHeight){
+        
+       addItems(2); 
+    }
+});
